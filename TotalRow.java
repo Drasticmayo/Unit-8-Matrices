3952fd087be7910c7a23f199cace3e5fa64d1391
Jude Armstrong <134647784+Drasticmayo@users.noreply.github.com>
2025-02-21 09:06:56 -0500

    Update TotalRow.java

--- TotalRow.java
+++ TotalRow.java
@@ -1,14 +1,26 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Jude A
 
 import java.util.*;
 
 public class TotalRow
 {
     public static List<Integer> getRowTotals( int[][] m )
+
     {
-    	//add code here
-		return null;
+        int rowTotal = 0;
+    	List<Integer> list = new ArrayList<Integer>();
+        for(int i = 0; i < m.length; i++)
+        {
+            for(int k = 0; k < m[i].length; k++)
+            {
+                rowTotal += m[i][k];
+            }
+            list.add(rowTotal);
+            rowTotal = 0;
+        }
+        
+		return list;
     }
 }
