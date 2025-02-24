3e533242aa7f4ee688efd9b96ea06ff341a8f849
Jude Armstrong <134647784+Drasticmayo@users.noreply.github.com>
2025-02-24 09:46:19 -0500

    Update Biggest.java

--- Biggest.java
+++ Biggest.java
@@ -1,11 +1,24 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Jude Armstrong
 
 public class Biggest
 {
     public static int getBig(int[][] m)
     {
-		return -1;
+        int inital = m[0][0];
+        
+        for (int i = 0; i < m.length; i++)
+        {
+            for (int k = 0; k < m[i].length; k++)
+            {
+                if (m[i][k] > inital)
+                {
+                    inital = m[i][k];
+                }
+            }
+
+        }
+		return inital;
     }
 }
